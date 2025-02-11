1fe7ec1eaca43537dd3c5e89d9bcc644b11572ff
joyjessica <jj1129@georgetown.edu>
2025-02-11 14:10:31 -0500

    upload dsan5100

--- multiclass-portfolio-website/build.sh
+++ multiclass-portfolio-website/build.sh
@@ -14,6 +14,33 @@ cd _site; for i in $(find  ./ -name .DS_Store); do rm $i; done; cd "$dir1"
 for i in $(find _site -type f); do chmod 644 $i; done
 for i in $(find _site -type d); do chmod 755 $i; done
 
+# GITHUB SYNC
+printf 'Would you like to push to GITHUB? (y/n)? '
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+
+    git config http.postBuffer 20242880000
+
+    # PULL CLOUD REPO TO LOCAL
+    git pull 
+
+    # SYNC TO LOCAL REPO TO CLOUD 
+    read -p 'ENTER MESSAGE: ' message
+    echo "commit message = "$message; 
+    git add ./; 
+    # MAIN BRANCH
+    git commit -m "$message"; 
+
+    # PUSH NON-MAIN BRANCH
+    #git push  -u origin w05-draft
+
+    # PUSH MAIN BRANCH
+    git push
+
+else
+    echo NOT PUSHING TO GTIHUB!
+fi
+
 # PUSH WEBSITE TO GU DOMAINS 
 printf 'Would you like to push to GU domains: (y/n)? '
 read answer
